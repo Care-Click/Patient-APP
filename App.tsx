a942b346a40a87d18bba9866281b2a7646542f0b
Adem-abderrazek <Adem11.abderrazek@gmail.com>
2024-04-23 13:51:56 +0100

    settig route for set profile

--- App.tsx
+++ App.tsx
@@ -3,17 +3,15 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Signin from './auth/Signin';
 import Signup from './auth/Signup';
+import Setprofile from './SetProfile';
 export default function App() {
   const Stack = createNativeStackNavigator();
-    
-
-    
-   
     return (
       <NavigationContainer>
         <Stack.Navigator>
           <Stack.Screen name="Sigin" component={Signin} />
           <Stack.Screen name="Signup" component={Signup} />
+          <Stack.Screen name="setprofile" component={Setprofile} />
 
         </Stack.Navigator>
       </NavigationContainer>
