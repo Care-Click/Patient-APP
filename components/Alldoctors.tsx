e1730fc54c50f9abeeec4592a97693af34df470d
OumaymaMansour <mansour.oumeima@gmail.com>
2024-04-26 11:42:06 +0100

    styling search for allDoctors

--- components/Alldoctors.tsx
+++ components/Alldoctors.tsx
@@ -5,13 +5,13 @@ import { useNavigation } from '@react-navigation/native'; // Import useNavigatio
 import { FontAwesome, MaterialCommunityIcons } from '@expo/vector-icons';
 
 
-const Alldoctors = ({ navigation }) => {
+const Alldoctors = ({navigation}:any) => {
   const [searchTerm, setSearchTerm] = useState('');
   const [doctors, setDoctors] = useState([]);
 
   const fetchDoctors = async (speciality) => {
     try {
-      const response = await axios.get(`http://192.168.1.108:3001/api/patients/search/${speciality}`);
+      const response = await axios.get(`http://192.168.137.125:3001/api/patients/search/${speciality}`);
       setDoctors(response.data);
       console.log(response.data)
     } catch (error) {
@@ -25,16 +25,16 @@ const Alldoctors = ({ navigation }) => {
   const renderDoctorItem = ({ item }) => (
   <TouchableOpacity style={styles.doctorItem}>
        <Image source={{ uri: item.profile_picture }} style={styles.doctorImage} />
-      
+       <Text style={styles.doctorName}>{item.FullName}</Text>
       <View style={styles.doctorInfo}>
-        <Text style={styles.doctorName}>{item.FullName}</Text>
-        <Text style={styles.doctorSpecialty}>{item.MedicalExp.speciality}</Text>
+        
+        {/* <Text style={styles.doctorSpecialty}>{item.MedicalExp.speciality}</Text> */}
       </View>
       <TouchableOpacity style={styles.detailButton} onPress={() => navigation.navigate('Doctordetail', { doctorId: item.id })}>
         <Text style={styles.detailButtonText}>Detail</Text>
       </TouchableOpacity>
       <FontAwesome name="phone" size={24} color="red" style={styles.phoneIcon} />
-      <MaterialCommunityIcons name="message-processing-outline" size={24} color="red" style={styles.phoneIcon}/>
+      {/* <MaterialCommunityIcons name="message-processing-outline" size={24} color="red" style={styles.phoneIcon}/> */}
   
     </TouchableOpacity>
     )
@@ -59,16 +59,13 @@ const Alldoctors = ({ navigation }) => {
 };
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-  },
+ 
   doctorContainer: {
     marginBottom: 10,
     padding: 10,
     backgroundColor: '#ffffff',
     borderRadius: 5,
-    elevation: 2,
+    
   },
   searchInput: {
     height: 40,
@@ -88,11 +85,11 @@ const styles = StyleSheet.create({
   },
   doctorItem: {
     backgroundColor: '#fff',
-    borderRadius: 15,
+    borderRadius: 17,
     padding: 10,
     flexDirection: 'row',
     alignItems: 'center',
-    marginBottom: 10,
+    marginBottom: 15,
     shadowColor: "#000",
     shadowOffset: {
       width: 0,
@@ -103,16 +100,17 @@ const styles = StyleSheet.create({
     elevation: 5,
   },
   doctorImage: {
-    width: 50,
-    height: 50,
+    width: 40,
+    height: 40,
     borderRadius: 25,
   },
   doctorInfo: {
-    marginLeft: 10,
-    flex: 1,
+    marginLeft: 2,
+     flex: 1,
+    
   },
   doctorName: {
-    fontSize: 16,
+    fontSize: 14,
     fontWeight: 'bold',
   },
   doctorSpecialty: {
@@ -129,7 +127,7 @@ const styles = StyleSheet.create({
     fontSize: 16,
   },
   phoneIcon: {
-    marginHorizontal: 10,
+    marginHorizontal: 12,
   },
   
 });
