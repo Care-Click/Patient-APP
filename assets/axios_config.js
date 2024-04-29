c19dc7c8e7003440afa51ef498c6f70beeeeecb6
aminelaarif <laarifamine5569@gmail.com>
2024-04-29 10:35:55 +0100

    crated axios intercepters

---
+++ assets/axios_config.js
@@ -0,0 +1,21 @@
+import axios from 'axios';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+
+
+
+
+ 
+   
+
+
+// const instance = axios.create();
+
+// instance.interceptors.request.use(config => {
+//   if (token) {
+//     config.headers = token
+//   }
+//   return config;
+// });
+
+// export default instance;
