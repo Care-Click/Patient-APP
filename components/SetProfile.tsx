3e7471657cdcd8faa0d3d204009f3b2ad78ad151
aminelaarif <laarifamine5569@gmail.com>
2024-04-29 10:35:39 +0100

    refactor set profile with use form

--- components/SetProfile.tsx
+++ components/SetProfile.tsx
@@ -1,33 +1,46 @@
-import React, { useEffect, useState } from "react";
+import { StyleSheet, View, Text,Button , TextInput, Image ,Pressable, ScrollView } from 'react-native';
+import { useForm, Controller } from "react-hook-form"
+import { useState } from 'react';
+import YupPassword from 'yup-password'
+import { yupResolver } from "@hookform/resolvers/yup";
+import { useRoute } from "@react-navigation/native";
 import DateTimePicker from "@react-native-community/datetimepicker";
-import {
-  StyleSheet,
-  View,
-  Text,
-  Image,
-  TextInput,
-  Pressable,
-} from "react-native";
 import * as Location from "expo-location";
 import { Dropdown } from "react-native-element-dropdown";
-import axios from "axios";
-const data = [
-  { label: " male", value: "male" },
-  { label: " female", value: "female" },
-  { label: " others", value: "others" },
-];
-Location.setGoogleApiKey("AIzaSyD5GUOMMrDY5Ml8JOQ5j7z7p9f8GaGCDBg");
-function Setprofile({ navigation, route }: any) {
-  const { email, password } = route.params;
+import * as yup from "yup";
+import axios from 'axios';
+
+YupPassword(yup)
+
+
+
+ function Signin({navigation}:any) {
+
+  interface inputs {
+    email:string,
+    password :string
+  }
+
+  const route = useRoute();
+  const { email,password  } :inputs = route.params;
+  const data = [
+    { label: " male", value: "male" },
+    { label: " female", value: "female" },
+    { label: " others", value: "others" },
+  ];
+
+
   const [selectedDate, setSelectedDate] = useState(new Date());
-  const [Fullname, setName] = useState("");
-  const [Phonenumber, setnumber] = useState("");
   const [genre, setgenre] = useState("");
   const [loca, setLocation] = useState({
     longitude: 0,
     latitude: 0,
     place: {},
   });
+
+
+
+
   const reverseGeocode = async () => {
     try {
       let currentLocation = await Location.getCurrentPositionAsync({});
@@ -49,72 +62,139 @@ function Setprofile({ navigation, route }: any) {
     }
   };
 
-  useEffect(() => {
-    reverseGeocode();
-  }, []);
 
-  const signpatient = async () => {
-    console.log("😎😎😎", {
-      email,
-      password,
-      selectedDate,
-      Fullname,
-      Phonenumber,
-      genre,
-      location: loca,
-    });
-    try {
-      const { data } = await axios.post(
-        "http://192.168.1.16:3000/api/patients/signup",
-        {
-          email,
-          password,
-          date_of_birth:selectedDate,
-          FullName:Fullname,
-          phone_number:Phonenumber,
-          Gender:genre,
-          location: loca,
-          profile_picture:""
-        }
-      );
-      console.log(data);
-    } catch (error) {
-      console.log(error);
-    }
-  };
   const handleDateChange = (event: any, date: any) => {
     if (date) {
       setSelectedDate(date);
     }
   };
 
+
+const schema = yup.object().shape({
+  FullName:yup.string().required("this field is required"),
+ PhoneNumber:yup.string().required("this field is required")
+})
+
+
+  type FormData = {
+    PhoneNumber: string
+    FullName: string
+  }
+
+  
+  interface inputs {
+    PhoneNumber: string
+    FullName: string
+  }
+
+
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+   
+  } = useForm<FormData>({
+    resolver:yupResolver(schema),
+    defaultValues: {
+      PhoneNumber: "",
+      FullName: "",
+    },
+  })
+  const onSubmit = async (inputs:inputs) => {
+    console.log("😎😎😎", {
+      email,
+      password,
+      selectedDate,
+      FullName:inputs.FullName,
+      phone_number:inputs.PhoneNumber,
+      genre,
+      location: loca,
+    });
+    try {
+      const { data } = await axios.post(
+        "http://192.168.1.21:3000/api/patients/signup",
+        {
+          email,
+          password,
+          date_of_birth:selectedDate,
+          FullName:inputs.FullName,
+          phone_number:inputs.PhoneNumber,
+          Gender:genre,
+          location: loca,
+          profile_picture:""
+        }
+      );
+      console.log(data);
+      navigation.navigate("Signin")
+    } catch (error) {
+      console.log(error);
+    }
+ 
+  }
+
   return (
-    <View style={styles.container}>
-      <View>
-        <Image style={styles.logo} source={require("../assets/logo.png")} />
-        <Text style={styles.name}>CareClick</Text>
-      </View>
-
-      <Text style={styles.title}>Fill your personal info </Text>
-
-      <Text style={styles.Fullname}>Full Name</Text>
-      <View>
-        <TextInput
-          style={styles.input}
-          onChangeText={(text) => setName(text)}
-          placeholder="    ex jack jones"
+    <View style={styles.maincontainer}>
+      <View >
+        <Image
+        style={styles.logo}
+        source={require("../assets/image/logo.png")}
         />
-      </View>
-      <Text style={styles.Fullname}>Phone Number</Text>
-      <View>
-        <TextInput
-          style={styles.input}
-          onChangeText={(text) => setnumber(text)}
-          placeholder="      ex jack jones"
-        />
-      </View>
-      <Text style={styles.Fullname}>Date of birth</Text>
-      <View>
+       
+        </View>
+
+        <View >
+        <Text style={styles.title} >Please Set your profile  </Text>
+        </View>
+        <Text style={styles.Email}>FullName</Text>
+      <Controller
+        control={control}
+        rules={{
+          required: {value:true, message :"This  field  is required" } , 
+        
+        }}
+        render={({ field: { onChange, onBlur, value } }) => (
+          <TextInput
+            placeholder="FullName"
+            onBlur={onBlur}
+            onChangeText={onChange}
+            value={value}
+            style={styles.input}
+          />
+        )}
+        name="FullName"
+      />
+      {errors.FullName && <Text>{errors.FullName.message}</Text>}
+      
+      <View style={styles.passwordcontainer} >
+      <Text style={styles.password}>
+          PhoneNumber 
+         </Text>
+      <Controller
+        control={control}
+        rules={{
+          maxLength: 100,
+          required: true,
+        }}
+        render={({ field: { onChange, onBlur, value } }) => (
+          <TextInput
+            placeholder="PhoneNumber"
+            onBlur={onBlur}
+            onChangeText={onChange}
+            value={value}
+            style={styles.input}
+           
+          />
+        )}
+        name="PhoneNumber"
+      />
+        {errors.PhoneNumber && <Text style={styles.errorpassword} >{errors.PhoneNumber.message}</Text>}
+</View>
+
+     
+       
+       
+
+       <View>
         <DateTimePicker
           value={selectedDate}
           mode="date"
@@ -122,7 +202,7 @@ function Setprofile({ navigation, route }: any) {
           onChange={handleDateChange}
         />
       </View>
-      <Text style={styles.Fullname}> Select a gender</Text>
+     
       <Dropdown
         style={styles.dropdown}
         placeholderStyle={styles.placeholderStyle}
@@ -131,114 +211,135 @@ function Setprofile({ navigation, route }: any) {
         maxHeight={300}
         labelField="label"
         valueField="value"
-        placeholder="Select genre"
+        placeholder="Select gendre"
         value={genre}
         onChange={(item) => {
           setgenre(item.value);
         }}
       />
-      <Pressable>
-        <Text
-          style={styles.button}
-          onPress={() => {
-            signpatient();
-          }}
-        >
-          {" "}
-          Sign up {" "}
-        </Text>
-        
-      </Pressable>
 
-      <Pressable
-            onPress={() => {
-              navigation.navigate("Signin");
-            }}
-          >
-            <Text>if you already have an account </Text>
-             <Text style={styles.navigation}>sign in</Text>
-             
-          </Pressable>
+<Button title="Submit" onPress={handleSubmit(onSubmit)} />
+
+<View style ={styles.position} >
+      <Text style={styles.account}>
+        Alraedy have an account?  <Pressable  onPress={()=>{navigation.navigate("Signin")}}   ><Text style={styles.navigation}>sign in</Text></Pressable>
+       </Text>
+       </View>
     </View>
-  );
-}
 
-export default Setprofile;
+    
+  )
+}
+export default Signin
 
 const styles = StyleSheet.create({
-  button: {
-    backgroundColor: "#1DBED3",
-    fontSize: 20,
-    borderRadius: 30,
-    textAlign: "center",
-    width: 200,
-    color: "#FFFFFF",
-  },
-  dropdown: {
-    margin: 16,
-    height: 50,
-    width: 150,
-    borderBottomColor: "gray",
-    borderBottomWidth: 0.5,
-  },
-
-  placeholderStyle: {
-    fontSize: 16,
-  },
-  selectedTextStyle: {
-    fontSize: 16,
-  },
-  label: {
-    fontWeight: "bold",
-    marginBottom: 5,
-  },
-  container: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-    marginTop: -150,
-  },
   logo: {
-    width: 150,
-    height: 200,
-    marginTop : 50
+   width : 150,
+   height : 200 ,
+   marginLeft : 130,
+   marginTop : -30
   },
-
-  title: {
-    fontWeight: "bold",
-    fontSize: 25,
+  title : {
+    marginLeft : 50,
+    marginTop: -40,
+    fontSize : 25,
+  },
+  Email : {
+    color : "grey",
+    marginRight : 240 ,
+    marginTop : 30
+  
   },
   name: {
-    paddingTop: 40,
-    color: "#F26268",
-    fontSize: 20,
-    fontWeight: "bold",
-  },
-
-  Fullname: {
-    marginRight: 170,
-    fontWeight: "bold",
-    color: "grey",
-  },
-
-  input: {
-    marginTop: 10,
+    marginLeft : 80,
+    marginTop : -80,
+    color : "#F26268" , 
+    fontSize : 20,
+    paddingBottom : 60
+  } , 
+  input : {
     height: 40,
+    margin: 12,
     borderWidth: 1,
-    borderRadius: 20,
-    width: 300,
-    color: "black",
-    marginBottom: 10,
-  },
-  containerdate: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-    paddingTop: -80,
-  },
-  navigation: {
-    color: "#1DBED3",
-    fontSize: 15,
+    padding: 10,
+     borderRadius : 20 , 
+     width : 300 , 
+    color : "black" ,
+     
     
   },
-});
+  container : {
+marginTop : 30
+  } , 
+container1 : {
+  marginTop : 50
+    },
+
+ navigation : {
+  color : "#1DBED3",
+  fontSize : 15,
+ } ,
+
+ position : {
+  marginTop : 50,
+  marginLeft : 20
+ },
+
+ account : {
+   color : "#888888",
+   fontSize : 15,
+ },
+
+ password : {
+ 
+  marginLeft : 15 ,
+  color : "grey" ,
+ },
+
+ maincontainer:{
+flex : 1 , 
+justifyContent : "center",
+alignItems:"center",
+marginTop : 10
+
+ },button: {
+  marginTop: 15,
+  marginLeft: 10,
+  height: 50,
+  width: 200,
+  backgroundColor: '#F26268',
+  borderRadius: 10,
+  elevation: 3, // for Android
+},
+buttonText: {
+  marginTop: 18,
+  color: 'white',
+  fontWeight: 'bold',
+  textTransform: 'uppercase',
+  fontSize: 16,
+  textAlign: 'center',
+},
+
+passwordcontainer :{
+marginTop : 20
+},
+ 
+errorpassword : {
+  marginLeft : 60
+},
+
+dropdown: {
+  margin: 16,
+  height: 50,
+  width: 150,
+  borderBottomColor: "gray",
+  borderBottomWidth: 0.5,
+},
+placeholderStyle: {
+  fontSize: 16,
+},
+selectedTextStyle: {
+  fontSize: 16,
+},
+    
+  });
