a6eb7c6d2ff627586ec7272d0f26a4fbe8ee1f92
Adem-abderrazek <Adem11.abderrazek@gmail.com>
2024-05-01 19:50:18 +0100

    fixing date to the right format

--- components/Requests.tsx
+++ components/Requests.tsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, StyleSheet, ScrollView, Pressable } from 'react-native';
-import axios from 'axios';
-import AsyncStorage from '@react-native-async-storage/async-storage';
+import { View, Text, StyleSheet, ScrollView, Pressable,Image } from 'react-native';
+import axiosInstance from '../assets/axios_config';
 
 interface Doctors {
   id: number | null
@@ -23,19 +22,36 @@ const Request = ({ navigation }: any) => {
 
 
   const GetRequests = async () => {
-    const id = await AsyncStorage.getItem('id');
 
     try {
-      const result = await axios(`http://192.168.10.7:3000/api/patients/getPatientRequests/${id}`)
+      const result = await axiosInstance(`http://192.168.1.11:3000/api/patients/getPatientRequests`)
       setData(result.data)
     } catch (error) {
       console.log(error)
     }
   }
-
+  const formatDateTime = (dateTimeString: string) => {
+    const daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    const monthsOfYear = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+    const dateTime = new Date(dateTimeString);
+    const dayOfWeek = daysOfWeek[dateTime.getDay()]; 
+    const monthOfYear = monthsOfYear[dateTime.getMonth()]; 
+    const year = dateTime.getFullYear(); 
+    const formattedTime = dateTime.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
+    return `${dayOfWeek}, ${monthOfYear} ${dateTime.getDate()}, ${year} at ${formattedTime}`;
+  };
   return (
+
     <ScrollView style={styles.scrollView}>
+      <View>
+        <Image
+          style={styles.logo}
+          source={require("../assets/image/logo.png")}
+        />
+        <Text style={styles.name}>CareClick</Text>
+      </View>
       <View style={styles.container}>
+        
         {data.map((element) => (
 
           <Pressable
@@ -53,8 +69,8 @@ const Request = ({ navigation }: any) => {
                   {element.Doctor && (
                     <Text style={styles.doctorName}>{element.Doctor.FullName}</Text>
                   )}
-                  <Text style={styles.date}>{element.createdAt}</Text>
                 </View>
+                  <Text style={styles.date}>{formatDateTime(element.createdAt)}</Text>
                 <Text style={[styles.status, element.status === "Accepted" ? styles.greenStatus : styles.redStatus]}>
                   {element.status}
                 </Text>
@@ -68,15 +84,34 @@ const Request = ({ navigation }: any) => {
 };
 
 const styles = StyleSheet.create({
+  logo: {
+    marginTop: 40,
+    width: 100,
+    height: 100,
+    
+  },
+  name: {
+    marginLeft: 70,
+    marginTop: -50,
+    color: "#F26268",
+    fontSize: 25,
+    paddingBottom: 60
+  },
   scrollView: {
     flex: 1,
     backgroundColor: '#F7F7F7',
+    
   },
   container: {
+    paddingTop:100,
+    flex:1,
+    justifyContent:"center",
     paddingVertical: 20,
     paddingHorizontal: 15,
   },
   gridContainer: {
+    
+    flexWrap:'wrap',
     backgroundColor: '#FFFFFF',
     marginBottom: 20,
     borderRadius: 8,
@@ -98,6 +133,7 @@ const styles = StyleSheet.create({
     marginTop: 10,
   },
   doctorName: {
+    
     fontWeight: 'bold',
     marginRight: 10,
     color: '#333',
