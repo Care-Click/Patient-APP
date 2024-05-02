e4013677d7cbbff2ff029c60bfeaa2c84aceb813
OumaymaMansour <mansour.oumeima@gmail.com>
2024-05-02 18:37:08 +0100

    some updates

--- assets/axios_config.js
+++ assets/axios_config.js
@@ -8,7 +8,7 @@ axiosInstance.interceptors.request.use(
     
     if (token) {
       
-      config.headers.Authorization = `Bearer ${token}`;
+      config.headers.Authorization = token;
     }
     return config;
   },
