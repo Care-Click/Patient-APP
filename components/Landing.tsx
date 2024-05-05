97980733cde6c246675a69b44150fe0d673e03a8
OumaymaMansour <mansour.oumeima@gmail.com>
2024-05-05 02:30:17 +0100

    styling the landing page for the patient

--- components/Landing.tsx
+++ components/Landing.tsx
@@ -1,18 +1,43 @@
-import React from 'react';
-import { View, Text, StyleSheet, Image } from 'react-native';
-//import LinearGradient from 'react-native-linear-gradient';
+import React from "react";
+import {
+  View,
+  Pressable,
+  Text,
+  StyleSheet,
+  ImageBackground,
+  Image,
+} from "react-native";
 
-const Landing = () => {
+
+const StyledButton = ({ onPress, title }) => {
   return (
-    <View
-     
-    >
-      <Image
-        source={require("../assets/image/Capture1.PNG")} 
+    <Pressable style={styles.button} onPress={onPress}>
+      <Text style={styles.text}>{title}</Text>
+    </Pressable>
+  );
+};
+
+const Landing = ({ navigation }) => {
+  return (
+    <View style={styles.container}>
+      <ImageBackground
+        source={require("../assets/image/Capture1.png")}
         style={styles.image}
-      />
-      <Text style={styles.header}>HOPE FOR HUMANITY</Text>
-      <Text style={styles.subHeader}>Welcome to hope for humanity</Text>
+      >
+        <Image
+          style={styles.logo}
+          source={require("../assets/image/logo.png")}
+        />
+        <View style={styles.overlay}>
+          <Text style={styles.header}>Emergency Care, Instantly</Text>
+          <Text style={styles.subHeader}>Welcome to CareClick</Text>
+
+          <StyledButton
+            title="Enter Here"
+            onPress={() => navigation.navigate("Signin")}
+          />
+        </View>
+      </ImageBackground>
     </View>
   );
 };
@@ -20,25 +45,60 @@ const Landing = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    padding: 24,
+    justifyContent: "center",
+    alignItems: "center",
   },
+  button: {
+    paddingVertical: 9,
+    paddingHorizontal: 20,
+    backgroundColor: "rgba(250, 55, 40, 0.7)",
+    width: 190,
+    borderRadius: 20,
+    alignItems: "center",
+    marginBottom: -60,
+  },
+  text: {
+    color: "white",
+    fontSize: 16,
+    fontWeight: "bold",
+  },
+  logo: {
+    width: 150,
+    height: 100,
+    marginTop: -360,
+    zIndex: 1,
+  },
+
   image: {
-    width: 100, 
-    height: 100, 
-    marginBottom: 20,
+    width: "100%",
+    height: "100%",
+    justifyContent: "center",
+    alignItems: "center",
   },
+
+  overlay: {
+    ...StyleSheet.absoluteFillObject,
+    backgroundColor:
+      "linear-gradient(0.01deg, #1DBED3 -1%, rgba(10, 107, 158, 0.801566) 59.1%, rgba(52, 167, 81, 0) 301.87%)", // Red color overlay with 50% opacity
+    justifyContent: "center",
+    alignItems: "center",
+  },
+
   header: {
-    fontSize: 24,
-    fontWeight: 'bold',
-    color: '#ffffff',
-    marginBottom: 12,
+    fontSize: 26,
+    fontWeight: "bold",
+    color: "#ffffff",
+    marginTop: 9,
   },
   subHeader: {
-    fontSize: 16,
-    color: '#ffffff',
+    fontSize: 26,
+    color: "#ffffff",
+    fontWeight: "bold",
+    marginBottom: 40,
+    marginVertical: 33,
+    marginLeft: 2,
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
-
 export default Landing;
